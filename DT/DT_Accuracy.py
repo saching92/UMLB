9f89964c83d02498c0af7594907dd00072a69c1d
saching92 <36415849+saching92@users.noreply.github.com>
2018-03-07 22:47:58 -0500

    Update DT_Accuracy.py

--- DT/DT_Accuracy.py
+++ DT/DT_Accuracy.py
@@ -1,49 +1,34 @@
+#!/usr/bin/python
+
+"""
+    This is the code to accompany the Lesson 3 (decision tree) mini-project.
+
+    Use a Decision Tree to identify emails from the Enron corpus by author:
+    Sara has label 0
+    Chris has label 1
+"""
+
 import sys
-from class_vis import prettyPicture
-from prep_terrain_data import makeTerrainData
+from time import time
 
-import matplotlib.pyplot as plt
-import numpy as np
-import pylab as pl
+sys.path.append("../tools/")
+from email_preprocess import preprocess
 
-features_train, labels_train, features_test, labels_test = makeTerrainData()
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-########################## DECISION TREE #################################
-
-
-### your code goes here--now create 2 decision tree classifiers,
-### one with min_samples_split=2 and one with min_samples_split=50
-### compute the accuracies on the testing data and store
-### the accuracy numbers to acc_min_samples_split_2 and
-### acc_min_samples_split_50, respectively
-
-from sklearn import tree
 from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
 
-#### your code goes here
+#########################################################
 
-
-clf = tree.DecisionTreeClassifier(min_samples_split=2)
-clf.fit(features_train, labels_train)   
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
 pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
+print "The Decision Tree accuracy is: %r" % acc
 
-acc_min_samples_split_2 = accuracy_score(pred, labels_test)
-### be sure to compute the accuracy on the test set
-print acc_min_samples_split_2
-
-clf = tree.DecisionTreeClassifier(min_samples_split=50)
-clf.fit(features_train, labels_train)   
-pred = clf.predict(features_test)
-
-acc_min_samples_split_50 = accuracy_score(pred, labels_test)
-### be sure to compute the accuracy on the test set
-print acc_min_samples_split_50
-
-
-
-
-def submitAccuracies():
-  return {"acc_min_samples_split_2":round(acc_min_samples_split_2,3),
-          "acc_min_samples_split_50":round(acc_min_samples_split_50,3)}
+print "number of features in our data is: %r" % len(features_train[0])
+#########################################################
